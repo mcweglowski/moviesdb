4d87bca92b2c69b576220857aedc68d028e93807
Marcin Weglowski <mcweglowski@gmail.com>
2019-12-08 23:07:04 +0100

    Very basic entities for Movies

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.IntegerField()
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateTimeField()
