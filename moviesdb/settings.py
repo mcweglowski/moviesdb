98d2bb0fa43350283c69d7f6e208f59e12f7ee24
Marcin Weglowski <mcweglowski@gmail.com>
2019-12-13 16:22:48 +0100

    Heroku configuration according to medium.com/agatha-codes

--- moviesdb/settings.py
+++ moviesdb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://movies-dpl-test.herokuapp.com/']
 
 
 # Application definition
