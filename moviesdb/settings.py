8f2b6c5cb715463d0a4944d0787feb9fc32a0239
Marcin Weglowski <marcin@escali.no>
2019-12-17 12:56:30 +0100

    Docker and docker-compose files work fine

--- moviesdb/settings.py
+++ moviesdb/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'moviesdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # should go to ENVVARS
+        'USER': 'postgres',  # should go to ENVVARS
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
