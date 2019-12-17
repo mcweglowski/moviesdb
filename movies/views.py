f79a202ce80119c9d2463eace43ab56a02545db6
Marcin Weglowski <marcin@escali.no>
2019-12-17 14:17:46 +0100

    Enable functional tests. Add temporarly chromedriver.exe unitl I will find a way to make it in a better way

--- movies/views.py
+++ movies/views.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Movies Site!</h1>')
+    return HttpResponse(f'''<head>
+                                <title>Movies DB</title>
+                            </head>
+                            <body>
+                                <h1>Movies Site!</h1>
+                            </body>''')
