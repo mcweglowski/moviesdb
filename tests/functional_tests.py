3955aca190fdb08732ea5cb3793887a99508fc1e
Marcin Weglowski <marcin@escali.no>
2019-12-17 14:58:12 +0100

    Elegant SmokeTest

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(executable_path=".\chromedriver.exe")
-browser.get("http://localhost:8000")
 
-assert 'Movies DB' in browser.title
+class SmokeTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_reach_hompeage_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Movies DB', self.browser.title)
+
+
+if '__main__' == __name__:
+    unittest.main(warnings='ignore')
