71135fe7a09ee7532a668016b0ccaa25d405b93d
Marcin Weglowski <marcin@escali.no>
2019-12-20 13:25:18 +0100

    Initial unit tests and initial functional tests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(executable_path=".\chromedriver.exe")
-browser.get("http://localhost:8000")
 
-assert 'Movies DB' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_webpage_start_smoke_pass(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Movies DB', self.browser.title)
+
+    def test_can_webpage_start_smoke_fail(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Movies DB2', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
