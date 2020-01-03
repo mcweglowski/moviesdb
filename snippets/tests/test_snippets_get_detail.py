2416d21c789023acff7f97bb2325991a57b4222d
Marcin Weglowski <mcweglowski@gmail.com>
2020-01-04 00:16:03 +0100

    Add basic test_view_returns_snippet_details GET. It should be extended to test not only status, but also a contnet

--- snippets/tests/test_snippets_get_detail.py
+++ snippets/tests/test_snippets_get_detail.py
@@ -1,18 +1,16 @@
 from django.test import TestCase
-# from rest_framework.request import Request
-# from snippets.models import Snippet
+from snippets.models import Snippet
 
 
 class SnippetGetDetailTest(TestCase):
-    # def test_view_returns_snippet_details(self):
-    #     snippet = Snippet()
-    #     snippet.code = "some code"
-    #     snippet.save()
+    def test_view_returns_snippet_details(self):
+        snippet = Snippet()
+        snippet.code = "some code"
+        snippet.save()
 
-    #     response = self.client.get(f'/snippets/{snippet.pk}',
-    #                                follow=True)
-
-    #
+        response = self.client.get(f'/snippets/', {'pk': snippet.pk},
+                                   follow=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_view_creates_snippet(self):
         response = self.client.post('/snippets/', {'code': '123'},
