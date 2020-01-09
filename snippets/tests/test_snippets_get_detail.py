bd7d4069617ea9f519e39bb63a63960c9b217e6d
Marcin Weglowski <marcin@escali.no>
2020-01-09 11:01:57 +0100

    Use setUp in SnippetGetDetailTest class

--- snippets/tests/test_snippets_get_detail.py
+++ snippets/tests/test_snippets_get_detail.py
@@ -5,48 +5,40 @@ from snippets.views import SnippetDetail
 
 
 class SnippetGetDetailTest(TestCase):
-    def test_view_get_snippet(self):
-        snippet = Snippet()
-        snippet.code = "some code"
-        snippet.save()
+    def setUp(self):
+        self.snippet = Snippet()
+        self.snippet.code = 'print("Hello World!")'
+        self.snippet.save()
 
-        response = self.client.get(f'/snippets/', {'pk': snippet.pk},
+    def test_view_get_snippet(self):
+        response = self.client.get(f'/snippets/', {'pk': self.snippet.pk},
                                    follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_view_put_snippet(self):
-        snippet = Snippet()
-        snippet.code = 'print("Hello World!")'
-        snippet.save()
-
-        expected_code = 'abc'
+        expected_code = 'foo="bar"'
 
         factory = APIRequestFactory()
         request = factory.put('/snippets/', {'code': expected_code})
         tested_view = SnippetDetail.as_view()
 
-        response = tested_view(request, pk=snippet.pk)
-
-        snippet = Snippet.objects.get(pk=snippet.pk)
+        response = tested_view(request, pk=self.snippet.pk)
+        snippet = Snippet.objects.get(pk=self.snippet.pk)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(snippet.code, expected_code)
 
     def test_view_delete_snippet(self):
-        snippet = Snippet()
-        snippet.code = 'foo="bar"\n'
-        snippet.save()
-
-        is_snippet = Snippet.objects.filter(pk=snippet.pk).exists()
+        is_snippet = Snippet.objects.filter(pk=self.snippet.pk).exists()
         self.assertTrue(is_snippet)
 
         factory = APIRequestFactory()
         request = factory.delete(f'/snippets/', format='json')
 
         tested_view = SnippetDetail.as_view()
-        response = tested_view(request, pk=snippet.pk)
+        response = tested_view(request, pk=self.snippet.pk)
 
-        is_snippet = Snippet.objects.filter(pk=snippet.pk).exists()
+        is_snippet = Snippet.objects.filter(pk=self.snippet.pk).exists()
 
         self.assertEqual(response.status_code, 204)
         self.assertFalse(is_snippet)
