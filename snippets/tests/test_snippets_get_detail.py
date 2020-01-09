fdcfc9c654b0fdd61d9a36171f6a665cfe9cf821
Marcin Weglowski <marcin@escali.no>
2020-01-09 08:40:38 +0100

    test_view_put_snippet still doesn't work

--- snippets/tests/test_snippets_get_detail.py
+++ snippets/tests/test_snippets_get_detail.py
@@ -16,16 +16,14 @@ class SnippetGetDetailTest(TestCase):
 
     def test_view_put_snippet(self):
         expected_code = 'print("Hello World!")'
-        response = self.client.post('/snippets/', {'code': f'{expected_code}'},
-                                    format='json')
 
-        response_pk = response.data['id']
-        snippet = Snippet.objects.get(pk=response_pk)
+        factory = APIRequestFactory()
+        request = factory.put('/snippets/1', {'code': expected_code})
+        tested_view = SnippetDetail.as_view()
+
+        response = tested_view(request)
 
         self.assertEqual(response.status_code, 201)
-        self.assertIsNotNone(snippet)
-        self.assertEqual(response.data['code'], expected_code)
-        self.assertEqual(snippet.code, expected_code)
 
     def test_view_delete_snippet(self):
         snippet = Snippet()
