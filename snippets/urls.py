3088f1d94ba5fc6d40c652fe7de617072c48c254
Marcin Weglowski <marcin@escali.no>
2020-01-20 15:20:38 +0100

    Endpoint for highlighting code

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,7 +6,8 @@ from snippets import views
 
 urlpatterns = [
     path('<int:pk>/', views.SnippetDetail.as_view(), name='snippets-detail'),
-    path('', views.SnippetList.as_view(), name='snippets-list'),
+    path('', views.api_root, name='snippets-root'),
+    path('<int:pk>/highlight/', views.SnippetHighlight.as_view()),
     path('users/', views.UserList.as_view(), name='users-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='users-detail'),
     path('api-auth/', include('rest_framework.urls')),
